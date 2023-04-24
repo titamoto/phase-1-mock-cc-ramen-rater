a5374fb0a144e30ba7fac6f99d645ed82c2dddec
Aleksandr Savchenko <titamoto@gmail.com>
2023-04-24 12:41:04 -0700

    complet core tasks

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
-// write your code here
+document.addEventListener("DOMContentLoaded", () => {
+    
+    document.querySelector("#new-ramen").addEventListener("submit", addNewRamen);
+});
+
+function addNewRamen(e) {
+    e.preventDefault();
+    console.log(e);
+    const newRamen = {
+        name: e.target[0].value,
+        restaurant: e.target[1].value,
+        image: e.target[2].value,
+        rating: e.target[3].value,
+        comment: e.target[4].value
+    }
+
+    showMenuRamen(newRamen);
+}
+
+function showMenuRamen(ramen) {
+const menuRamen = document.createElement("span");
+menuRamen.className = "menu-image-container";
+menuRamen.innerHTML = `<img src="${ramen.image}" class="menu-image">`;
+
+document.querySelector("#ramen-menu").appendChild(menuRamen);
+
+menuRamen.querySelector("img").addEventListener("click", () => {
+    document.querySelector(".detail-image").src = `${ramen.image}`;
+    document.querySelector(".name").textContent = `${ramen.name}`;
+    document.querySelector(".restaurant").textContent = `${ramen.restaurant}`;
+    document.querySelector("#rating-display").textContent = `${ramen.rating}`;
+    document.querySelector("#comment-display").textContent = `${ramen.comment}`;
+});
+}
+
+
+function getRamenMenu() {
+    fetch("http://localhost:3000/ramens")
+    .then(resp => resp.json())
+    .then(ramens => ramens.forEach(ramen => showMenuRamen(ramen)));
+}
+
+getRamenMenu();
